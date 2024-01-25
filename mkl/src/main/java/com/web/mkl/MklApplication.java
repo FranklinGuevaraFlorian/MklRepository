9f4f8c48a3c9a30a2837ed53e43623e6a5b5d86c
Franklin Guevara <122811251+FranklinGuevaraFlorian@users.noreply.github.com>
2024-01-25 01:23:05 -0500

    Creando estructura del pyoyecto con SpringBoot

---
+++ mkl/src/main/java/com/web/mkl/MklApplication.java
@@ -0,0 +1,13 @@
+package com.web.mkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MklApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MklApplication.class, args);
+	}
+
+}
---
+++ mkl/src/test/java/com/web/mkl/MklApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.mkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MklApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
